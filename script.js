8012284a5fc5998a29b734e9b8c97685c0a08ec7
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-27 10:56:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateTimer() {
+	const timerElement = document.getElementById("timer");
+	const now = new Date();
+	const formattedTime = now.toLocaleString(); 
+	timerElement.textContent = formattedTime;
+}
+
+setInterval(updateTimer, 1000);
+
+updateTimer();
